d2de7ade75b981b228bcfed53c64335688d2e090
emaeroles <emanueleroles@gmail.com>
2023-02-13 12:39:07 -0300

    Creacion de entity Cliente, Response y princpio de Commnads Cliente

---
+++ Application/Wrappers/Response.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wrappers
+{
+    public class Response<T>
+    {
+        public Response()
+        {
+
+        }
+
+        public Response(T dada, string message = null)
+        {
+            Succeeded = true;
+            Message = message;
+            Data = dada;
+        }
+
+        public Response(string message)
+        {
+            Succeeded = false;
+            Message = message;
+        }
+
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+    }
+}
---
+++ Domain/Entities/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
