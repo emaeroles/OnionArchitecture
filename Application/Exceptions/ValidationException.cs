e9f2fca1e05e7c76997a00c39dfc778a479f0ddc
emaeroles <emanueleroles@gmail.com>
2023-02-13 12:13:52 -0300

    ValidationBehavior y ValidationException

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+
+namespace Application.Exceptions
+{
+    internal class ValidationException : Exception
+    {
+        public List<string> Errors { get; }
+        public ValidationException() : base("Se han producide uno o más errores de validación")
+        {
+            Errors = new List<string>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+
+        }
+    }
+}
